8f3c6aec718ddd209a17ba866f567a8fc28930c3
Thaylor Carvalho <thaylorcs@outlook.com>
2021-05-01 18:53:20 -0300

    Handling Calculator entries

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,7 @@
 class CalcController {
     constructor() {
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -22,19 +23,79 @@ class CalcController {
         }, 30000)
     }
 
-    addEventListenerAll(element, events, fn){
+    clearAll() {
+        this._operation = [];
+    }
+
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    addOperation(value) {
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value) {
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                break;
+            case 'subtracao':
+                break;
+            case 'divisao':
+                break;
+            case 'multiplicacao':
+                break;
+            case 'porcento':
+                break;
+            case 'igual':
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            
+            default: 0;
+                this.setError();
+                break;
+        }
+    }
+
+    addEventListenerAll(element, events, fn) {
 
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
         });
     }
 
-    initButtonsEvents(){
+    initButtonsEvents() {
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-        buttons.forEach((btn, index)=> {
+        buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
@@ -42,10 +103,10 @@ class CalcController {
             });
         });
 
-        
+
     }
 
-    setDisplayDateTime(){
+    setDisplayDateTime() {
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
             month: "long",
